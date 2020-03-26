678f17272ba399eb3f2e8f76f6a646061bb40b22
bessam-haj-salem <bessam-haj-salem@gmail.com>
2020-03-26 15:11:10 +0100

    phase 4

--- for-while-extra-material.js
+++ for-while-extra-material.js
@@ -6,16 +6,24 @@ hint: strings also have the .length property - i.e `"starburst".length` is 9
 
 // YOUR CODE GOES HERE
 
-function (arr){
-
+function longestString(arr){
+	var result = arr[0];
+	for( var i = 0 ; i < arr.length ; i++){
+		if(result.length < arr[i].length){
+			result = arr[i];
+		}
+	}
+	return result;
 }
 
 // Will you make it?
 // You were camping with your friends far away from home, but when it's time to go back,
 //  you realize that you fuel is running out and the nearest pump is 50 miles away! You know that on average,
 //   your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, 
-//   write a function that tells you if it is possible to get to the pump or not. Function should return true if it is possible and false if not.
+//   write a function that tells you if it is possible to get to the pump or not. Function should return true if it is possible and false
+// if not.
 //    The input values are always positive.
+
 
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 };
